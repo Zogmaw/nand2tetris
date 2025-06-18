142db11889ca2a4f72f1f2033331ca926f23e09b
ZRThornton <zach15682@gmail.com>
2025-06-18 08:17:32 -0400

    Complete Mult.asm

--- nand2tetris/projects/4/mult/Mult.asm
+++ nand2tetris/projects/4/mult/Mult.asm
@@ -7,4 +7,41 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
 
-//// Replace this comment with your code.
+//initialize R2 to 0
+@R2
+M=0
+
+//Get the number of times we'll interate through our loop
+@R0
+D=M
+
+//Store that in i
+@i
+M=D
+
+//Skip if i (R0) = 0
+@END
+D;JLE
+
+//Define our loop
+(LOOP)
+  //Get R1
+  @R1
+  D=M
+
+  //R2 = R2 + R1
+  @R2
+  M=M+D
+
+  //Decrement i
+  @i
+  M=M-1
+  D=M
+
+  //if i > 0, loop again
+  @LOOP
+  D;JGT
+
+(END)
+  @END
+  0;JMP
