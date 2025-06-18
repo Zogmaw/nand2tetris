6d750d045291ece53ed01efda4a00467608cc236
ZRThornton <zach15682@gmail.com>
2025-06-18 19:03:00 -0400

    finish Fill.asm

--- nand2tetris/projects/4/fill/Fill.asm
+++ nand2tetris/projects/4/fill/Fill.asm
@@ -8,4 +8,77 @@
 // i.e. writes "black" in every pixel. When no key is pressed, 
 // the screen should be cleared.
 
-//// Replace this comment with your code.
+@8191
+D=A
+@maxwords
+M=D
+
+(RESET)
+  //Set the current word to 0
+  @word
+  M=0
+
+  @SCREEN
+  D=A
+  @addr //reset ptr to SCREEN
+  M=D
+
+  //Check for input
+  @CHECKINPUT
+  0;JMP
+
+(CHECKINPUT)
+  @KBD
+  D=M //store input in D
+
+  @FILL //Fill if input
+  D;JGT
+
+  @EMPTY
+  D;JLE //Fill if no input
+
+(FILL)
+  //Move to the current address in addr
+  @addr
+  A=M
+  M=-1 //set to all 1s
+
+  @addr //Move to next 16 bits
+  M=M+1
+
+  @word //increase word count
+  M=M+1
+  D=M
+
+  //Check if we've written all words. If not, check input again. Else, reset our counters to the top.
+  @maxwords
+  D=M-D
+
+  @CHECKINPUT
+  D;JGT
+
+  @RESET
+  D;JLE
+
+(EMPTY)
+  //Move to the current address in addr
+  @addr
+  A=M
+  M=0 //set to all 0s
+
+  @addr //Move to next 16 bits
+  M=M+1
+
+  @word //increase word count
+  M=M+1
+  D=M
+
+  //Check if we've written all words. If not, check input again. Else, reset our counters to the top.
+  @maxwords
+  D=M-D
+
+  @CHECKINPUT
+  D;JGT
+
+  @RESET
+  D;JLE
